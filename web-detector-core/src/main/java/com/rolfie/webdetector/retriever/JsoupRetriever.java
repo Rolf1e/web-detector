85af1fe654d5f3f9b0362fd664a86c6780a0a2fa
Rolf1e <tigran.slama@laposte.net>
2020-02-23 11:52:33 +0100

    Fix Retriever from Jsoup

--- web-detector-core/src/main/java/com/rolfie/webdetector/retriever/JsoupRetriever.java
+++ web-detector-core/src/main/java/com/rolfie/webdetector/retriever/JsoupRetriever.java
@@ -2,14 +2,12 @@ package com.rolfie.webdetector.retriever;
 
 import com.rolfie.webdetector.retriever.infra.html.Line;
 import com.rolfie.webdetector.retriever.infra.html.LineNumber;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.nodes.Node;
-import org.jsoup.select.Elements;
 
-import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class JsoupRetriever implements WebRetriever {
@@ -17,6 +15,7 @@ public class JsoupRetriever implements WebRetriever {
     private static WebRetriever instance = null;
 
     private Document document;
+    private Integer lineCount;
 
     public static WebRetriever getInstance(Document document) {
         if (instance == null) {
@@ -41,13 +40,33 @@ public class JsoupRetriever implements WebRetriever {
 
     private Map<LineNumber, Line> mappingContent(Element toMap) {
         Map<LineNumber, Line> mappedContent = new HashMap<>();
-        int i = 0;
-        //TODO : parsing through nodes to get line by line
-
-        for (Node child : toMap.childNodes()) {
-            mappedContent.put(LineNumber.create(String.valueOf(i++)), Line.create(child.outerHtml()));
+        lineCount = 0;
+        for (Node node : toMap.childNodes()) {
+            mappedContent.putAll(getChilds(node));
         }
-
         return mappedContent;
     }
+
+    private Map<LineNumber, Line> getChilds(Node father) {
+        Map<LineNumber, Line> childs = new HashMap<>();
+        if(father.childNodes().isEmpty()){
+            childs.put(LineNumber.create(String.valueOf(lineCount++)), Line.create(father.outerHtml()));
+            return childs;
+        }
+        childs.putAll(getSubChilds(father));
+        return childs;
+    }
+
+    private Map<LineNumber, Line> getSubChilds(Node father) {
+        Map<LineNumber, Line> childs = new HashMap<>();
+        for (Node child : father.childNodes()) {
+            final List<Node> subChildNode = child.childNodes();
+            if (subChildNode.isEmpty()) {
+                childs.put(LineNumber.create(String.valueOf(lineCount++)), Line.create(child.outerHtml()));
+            }
+            childs.putAll(getChilds(child));
+        }
+        return childs;
+    }
+
 }
