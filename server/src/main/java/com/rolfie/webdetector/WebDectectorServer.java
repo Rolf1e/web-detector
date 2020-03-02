de6ec8245f37f695e729fbdabde789792e50a2ee
rolf1e <tigran.slama@laposte.net>
2020-03-02 18:01:52 +0100

    Maven config for jar file

--- server/src/main/java/com/rolfie/webdetector/WebDectectorServer.java
+++ server/src/main/java/com/rolfie/webdetector/WebDectectorServer.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserInterface {
+public class WebDectectorServer {
     public static void main(String[] args) {
-        SpringApplication.run(UserInterface.class, args);
+        SpringApplication.run(WebDectectorServer.class, args);
     }
 
 }
